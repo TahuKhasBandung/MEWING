5af8ded811dec878bf782d00890ec333e2b44ccc
TahuKhasBandung <tahuvakwan@gmail.com>
2024-11-15 00:31:25 +0700

    Update warp.sh

--- warp.sh
+++ warp.sh
@@ -33,21 +33,21 @@ red() { echo -e "\\033[32;1m${*}\\033[0m"; }
 ipsaya=$(curl -sS ipv4.icanhazip.com)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/rxztore/zta/main/permission"
+data_ip="https://raw.githubusercontent.com/TahuKhasBandung/zta/main/permission"
 checking_sc() {
   useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
   if [[ $date_list < $useexp ]]; then
     echo -ne
   else
     echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
-    echo -e "\033[42m          ZXSTORE          \033[0m"
+    echo -e "\033[42m          BLACKDARK Tunnel         \033[0m"
     echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
     echo -e ""
     echo -e "            ${RED}PERMISSION DENIED !${NC}"
     echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
     echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
     echo -e "             \033[0;33mContact Admin :${NC}"
-    echo -e "      ${GREEN}Telegram${NC} t.me/RiivKiii"
+    echo -e "      ${GREEN}Telegram${NC} t.me/rannv"
     echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
     exit 0
   fi
